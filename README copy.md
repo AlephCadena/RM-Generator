23f4a5c06ca4e6d40ab6c9a28dd719628427b9d6
Aleph Cadena <alephcadena@gmail.com>
2021-10-13 23:14:43 -0500

    finished README generator

---
+++ README copy.md
@@ -0,0 +1,33 @@
+# README Generator
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contribution](#Contribution)
+* [Testing](#Testing)
+* [Notes](#Notes)
+
+## Description
+User input is taken and with it, a README file is generated.
+
+## Installation
+``` 
+npm i inquirer
+```
+## Usage 
+[Please see how this README file was created](https://watch.screencastify.com/v/D1kxYcBsp0PPe5ZTGZAp)
+
+## Contribution
+[Contributor Covenant](https://www.contributor-covenant.org/)
+
+## Testing
+Unless the inquirer module is installed, this generator does not work.
+
+## Notes
+If you have any questions or concerns, feel free to reach out to me at . You can also reach out to me @AlephCadena on github.
+
+
---
+++ index.js
@@ -0,0 +1,84 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMd = require("./utils/generateMarkdown.js");
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Please give a short description of your project.",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What modules need to be installed?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What is the path or link to a demonstration of your project?",
+        name: "usageDemo",
+    },
+    {
+        type:"input",
+        message:'Please enter a short description of the demonstration of your project.',
+        name: "usageAlt"
+    },
+    {
+        type: "input",
+        message: "What contribution guidelines would you like to set? Please note the default is the 'Contributor Convenant'.",
+        name: "contribution",
+    },
+    {
+        type: "input",
+        message: "Please explain what your libraries do and how to test them.",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "githubUsername",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: "list",
+        message: "What license does your project use?",
+        name: "license",
+        choices: [
+            "None",
+            "Apache License 2.0",
+            "Boost Software License",
+            'BSD 2-Clause License',
+            'BSD 3-Clause License',
+            "CC0 1.0 Universal",
+            "Eclipse Public License 2.0",
+            "GNU GPL v2",
+            "GNU GPL v3",
+            "GNU AGPL v3",
+            "GNU LGPL v3",
+            "MIT License",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+        ],
+    },
+];
+
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        fs.writeFile('README.md', generateMd(response), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });
+    });
+}
+
+init();
